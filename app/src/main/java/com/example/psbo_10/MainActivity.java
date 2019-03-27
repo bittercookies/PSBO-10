857454568125c3ce50c3a730fcf59de4decb4add
Fia <flaw.riette@gmail.com>
2019-03-28 01:26:03 +0700

    login & register thing

--- app/src/main/java/com/example/psbo_10/MainActivity.java
+++ app/src/main/java/com/example/psbo_10/MainActivity.java
@@ -1,60 +1,78 @@
 package com.example.psbo_10;
 
-import android.content.Context;
+import com.example.psbo_10.activity.LoginActivity;
+import com.example.psbo_10.helper.SQLiteHandler;
+import com.example.psbo_10.helper.SessionManager;
+
+import java.util.HashMap;
+
+import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.psbo_10.R;
+public class MainActivity extends Activity {
 
-public class MainActivity extends AppCompatActivity {
+    private TextView txtName;
+    private TextView txtEmail;
+    private Button btnLogout;
 
-    Button btn_logout;
-    TextView txt_id, txt_username;
-    String id, username;
-    SharedPreferences sharedpreferences;
-
-    public static final String TAG_ID = "id";
-    public static final String TAG_USERNAME = "username";
+    private SQLiteHandler db;
+    private SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txt_id = (TextView) findViewById(R.id.txt_id);
-        txt_username = (TextView) findViewById(R.id.txt_username);
-        btn_logout = (Button) findViewById(R.id.btn_logout);
+        txtName = (TextView) findViewById(R.id.name);
+        txtEmail = (TextView) findViewById(R.id.email);
+        btnLogout = (Button) findViewById(R.id.btnLogout);
 
-        sharedpreferences = getSharedPreferences(Login.my_shared_preferences, Context.MODE_PRIVATE);
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
 
-        id = getIntent().getStringExtra(TAG_ID);
-        username = getIntent().getStringExtra(TAG_USERNAME);
+        // session manager
+        session = new SessionManager(getApplicationContext());
 
-        txt_id.setText("ID : " + id);
-        txt_username.setText("USERNAME : " + username);
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
 
-        btn_logout.setOnClickListener(new View.OnClickListener() {
+        // Fetching user details from sqlite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        txtName.setText(name);
+        txtEmail.setText(email);
+
+        // Logout button click event
+        btnLogout.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                // update login session ke FALSE dan mengosongkan nilai id dan username
-                SharedPreferences.Editor editor = sharedpreferences.edit();
-                editor.putBoolean(Login.session_status, false);
-                editor.putString(TAG_ID, null);
-                editor.putString(TAG_USERNAME, null);
-                editor.commit();
-
-                Intent intent = new Intent(MainActivity.this, Login.class);
-                finish();
-                startActivity(intent);
+                logoutUser();
             }
         });
+    }
 
+    /**
+     * Logging out the user. Will set isLoggedIn flag to false in shared
+     * preferences Clears the user data from sqlite users table
+     * */
+    private void logoutUser() {
+        session.setLogin(false);
+
+        db.deleteUsers();
+
+        // Launching the login activity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/psbo_10/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.psbo_10.app;
+
+public class AppConfig {
+    // Server user activity_login url
+    public static String URL_LOGIN = "http://192.168.137.1/android/psbo_10/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.137.1/android/psbo_10/register.php";
+}
