6a37dc8b2d56f25565357a17901bf653ace32b9e
Fia <flaw.riette@gmail.com>
2019-03-27 19:14:35 +0700

    Login Function

---
+++ app/src/main/java/com/example/psbo_10/Server.java
@@ -0,0 +1,5 @@
+package com.example.psbo_10;
+
+public class Server {
+    public static final String URL = "http://10.0.2.2/android/psbo_10/";
+}
---
+++ app/src/main/java/com/example/psbo_10/User.java
@@ -0,0 +1,17 @@
+package com.example.psbo_10;
+
+public class User {
+    private Integer user_id;
+    private String user_name;
+    private String user_pass;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String pass) {
+        user_id = id;
+        user_name = name;
+        user_pass = pass;
+
+        }
+}
