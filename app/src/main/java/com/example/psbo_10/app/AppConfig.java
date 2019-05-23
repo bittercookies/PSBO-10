7e709d20252e227a8916e725526125a5140bb35b
sabilra <salsabilaramadhani4@gmail.com>
2019-05-24 00:30:32 +0700

    fri

--- app/src/main/java/com/example/psbo_10/app/AppConfig.java
+++ app/src/main/java/com/example/psbo_10/app/AppConfig.java
@@ -2,8 +2,8 @@ package com.example.psbo_10.app;
 
 public class AppConfig {
     // Server user activity_login url
-    public static String URL_LOGIN = "http://10.0.2.2/android/psbo_10/login.php";
+    public static String URL_LOGIN = "http://10.0.bg2.bg2/android/psbo_10/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://10.0.2.2/android/psbo_10/register.php";
+    public static String URL_REGISTER = "http://10.0.bg2.bg2/android/psbo_10/register.php";
 }
